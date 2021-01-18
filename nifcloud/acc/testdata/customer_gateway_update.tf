6baa0c1d9372184f1bcbd5764cba218c4b27ca85
tily <tidnlyam@gmail.com>
2021-01-18 22:45:58 +0900

    Add acceptance tests for customer gateway resource

---
+++ nifcloud/acc/testdata/customer_gateway_update.tf
@@ -0,0 +1,13 @@
+provider "nifcloud" {
+  region = "jp-east-2"
+}
+
+resource "nifcloud_customer_gateway" "basic" {
+  type                               = "IPsec"
+  ip_address                         = "192.168.0.1"
+  bgp_asn                            = 65000
+  nifty_customer_gateway_name        = "cgw001updated"
+  nifty_lan_side_ip_address          = "192.168.0.1"
+  nifty_lan_side_cidr_block          = "192.168.0.0/28"
+  nifty_customer_gateway_description = "memoupdated"
+}
