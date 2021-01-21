61d66169448e15be70390d2114ad507376b74df5
tily <tidnlyam@gmail.com>
2021-01-21 09:42:59 +0900

    Remove unnecessary field bgp_asn in customer gateway resource documents and examples

--- examples/customer_gateway/main.tf
+++ examples/customer_gateway/main.tf
@@ -13,7 +13,6 @@ provider "nifcloud" {
 resource "nifcloud_customer_gateway" "example" {
   type                               = "IPsec"
   ip_address                         = "192.168.0.1"
-  bgp_asn                            = 65000
   nifty_customer_gateway_name        = "cgw002"
   nifty_lan_side_ip_address          = "192.168.0.1"
   nifty_lan_side_cidr_block          = "192.168.0.0/28"
