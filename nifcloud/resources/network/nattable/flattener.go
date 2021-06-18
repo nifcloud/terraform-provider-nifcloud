757a780f0d6b868fe52674a6dcb3bf6612b0ca52
fuku <fukuzawa.makoto@fjct.fujitsu.com>
2021-06-18 15:08:42 +0900

    resource/nifcloud_nat_table: Fix required field to always be saved

--- nifcloud/resources/network/nattable/flattener.go
+++ nifcloud/resources/network/nattable/flattener.go
@@ -40,27 +40,30 @@ func flatten(d *schema.ResourceData, res *computing.NiftyDescribeNatTablesRespon
 				}
 			}
 
+			snat := map[string]interface{}{
+				"rule_number":    r.RuleNumber,
+				"description":    r.Description,
+				"protocol":       r.Protocol,
+				"source_address": r.Source.Address,
+			}
+
+			if nifcloud.StringValue(r.Protocol) != "ALL" && nifcloud.StringValue(r.Protocol) != "ICMP" {
+				snat["source_port"] = nifcloud.Int64Value(r.Source.Port)
+				snat["translation_port"] = nifcloud.Int64Value(r.Translation.Port)
+			}
+
 			if findElm != nil {
-				snat := map[string]interface{}{
-					"rule_number":    r.RuleNumber,
-					"description":    r.Description,
-					"protocol":       r.Protocol,
-					"source_address": r.Source.Address,
-				}
-
-				if findElm["protocol"] != "ALL" && findElm["protocol"] != "ICMP" {
-					snat["source_port"] = nifcloud.Int64Value(r.Source.Port)
-					snat["translation_port"] = nifcloud.Int64Value(r.Translation.Port)
-				}
-
 				if findElm["outbound_interface_network_id"] != "" {
 					snat["outbound_interface_network_id"] = nifcloud.StringValue(r.OutboundInterface.NetworkId)
-				} else {
-					snat["outbound_interface_network_name"] = nifcloud.StringValue(r.OutboundInterface.NetworkName)
 				}
 
-				snats = append(snats, snat)
+				if findElm["outbound_interface_network_name"] != "" {
+					snat["outbound_interface_network_name"] = nifcloud.StringValue(r.OutboundInterface.NetworkName)
+				}
+			} else {
+				snat["outbound_interface_network_id"] = nifcloud.StringValue(r.OutboundInterface.NetworkId)
 			}
+			snats = append(snats, snat)
 
 		} else if nifcloud.StringValue(r.NatType) == "dnat" {
 			var findElm map[string]interface{}
@@ -73,27 +76,30 @@ func flatten(d *schema.ResourceData, res *computing.NiftyDescribeNatTablesRespon
 				}
 			}
 
+			dnat := map[string]interface{}{
+				"rule_number":         r.RuleNumber,
+				"description":         r.Description,
+				"protocol":            r.Protocol,
+				"translation_address": r.Translation.Address,
+			}
+
+			if nifcloud.StringValue(r.Protocol) != "ALL" && nifcloud.StringValue(r.Protocol) != "ICMP" {
+				dnat["destination_port"] = nifcloud.Int64Value(r.Destination.Port)
+				dnat["translation_port"] = nifcloud.Int64Value(r.Translation.Port)
+			}
+
 			if findElm != nil {
-				dnat := map[string]interface{}{
-					"rule_number":         r.RuleNumber,
-					"description":         r.Description,
-					"protocol":            r.Protocol,
-					"translation_address": r.Translation.Address,
-				}
-
-				if findElm["protocol"] != "ALL" && findElm["protocol"] != "ICMP" {
-					dnat["destination_port"] = nifcloud.Int64Value(r.Destination.Port)
-					dnat["translation_port"] = nifcloud.Int64Value(r.Translation.Port)
-				}
-
 				if findElm["inbound_interface_network_id"] != "" {
 					dnat["inbound_interface_network_id"] = nifcloud.StringValue(r.InboundInterface.NetworkId)
-				} else {
-					dnat["inbound_interface_network_name"] = nifcloud.StringValue(r.InboundInterface.NetworkName)
 				}
 
-				dnats = append(dnats, dnat)
+				if findElm["inbound_interface_network_name"] != "" {
+					dnat["inbound_interface_network_name"] = nifcloud.StringValue(r.InboundInterface.NetworkName)
+				}
+			} else {
+				dnat["inbound_interface_network_id"] = nifcloud.StringValue(r.InboundInterface.NetworkId)
 			}
+			dnats = append(dnats, dnat)
 		}
 	}
 
