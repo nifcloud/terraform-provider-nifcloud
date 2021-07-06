9eb46e7e4448232bc7a7cc495c5406486da695e3
fuku <fukuzawa.makoto@fjct.fujitsu.com>
2021-07-06 10:08:56 +0900

    resource/nifcloud_db_security_group: Fix to save rule in state

--- nifcloud/resources/rdb/dbsecuritygroup/flattener.go
+++ nifcloud/resources/rdb/dbsecuritygroup/flattener.go
@@ -28,45 +28,19 @@ func flatten(d *schema.ResourceData, res *rdb.DescribeDBSecurityGroupsResponse) 
 
 	if len(dbSecurityGroup.IPRanges) != 0 {
 		for _, r := range dbSecurityGroup.IPRanges {
-			var findElm map[string]interface{}
-
-			for _, e := range d.Get("rule").(*schema.Set).List() {
-				elm := e.(map[string]interface{})
-
-				if elm["cidr_ip"] == nifcloud.StringValue(r.CIDRIP) {
-					findElm = elm
-					break
-				}
+			rule := map[string]interface{}{
+				"cidr_ip": r.CIDRIP,
 			}
-
-			if findElm != nil {
-				rule := map[string]interface{}{
-					"cidr_ip": r.CIDRIP,
-				}
-				rules = append(rules, rule)
-			}
+			rules = append(rules, rule)
 		}
 	}
 
 	if len(dbSecurityGroup.EC2SecurityGroups) != 0 {
 		for _, r := range dbSecurityGroup.EC2SecurityGroups {
-			var findElm map[string]interface{}
-
-			for _, e := range d.Get("rule").(*schema.Set).List() {
-				elm := e.(map[string]interface{})
-
-				if elm["security_group_name"] == nifcloud.StringValue(r.EC2SecurityGroupName) {
-					findElm = elm
-					break
-				}
+			rule := map[string]interface{}{
+				"security_group_name": r.EC2SecurityGroupName,
 			}
-
-			if findElm != nil {
-				rule := map[string]interface{}{
-					"security_group_name": r.EC2SecurityGroupName,
-				}
-				rules = append(rules, rule)
-			}
+			rules = append(rules, rule)
 		}
 	}
 
