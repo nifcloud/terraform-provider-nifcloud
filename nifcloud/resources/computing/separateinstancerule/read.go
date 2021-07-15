cc8a24f80e0009ef293c660553f3323647bf689e
matsuoka-k-git <shuzomatsuokapeople@gmail.com>
2021-07-15 13:37:02 +0900

    Fix separate instance read conditional expression

--- nifcloud/resources/computing/separateinstancerule/read.go
+++ nifcloud/resources/computing/separateinstancerule/read.go
@@ -2,10 +2,8 @@ package separateinstancerule
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
-	"github.com/aws/aws-sdk-go-v2/aws/awserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/nifcloud/terraform-provider-nifcloud/nifcloud/client"
@@ -20,11 +18,6 @@ func read(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Di
 	res, err := req.Send(ctx)
 
 	if err != nil {
-		var awsErr awserr.Error
-		if errors.As(err, &awsErr) && awsErr.Code() == "Client.InvalidParameterNotFound.Instance" {
-			d.SetId("")
-			return nil
-		}
 		return diag.FromErr(fmt.Errorf("failed reading: %s", err))
 	}
 
