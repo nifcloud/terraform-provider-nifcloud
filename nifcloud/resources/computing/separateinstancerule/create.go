8d1ff2430dcdce2a4b95be665ee935cd87fd8e7b
tunakyonn <tunakyonn@gmail.com>
2021-07-29 18:03:03 +0900

    Add sleep for separateinstancerule create

--- nifcloud/resources/computing/separateinstancerule/create.go
+++ nifcloud/resources/computing/separateinstancerule/create.go
@@ -3,11 +3,14 @@ package separateinstancerule
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/nifcloud/terraform-provider-nifcloud/nifcloud/client"
 )
+
+const waiterInitialDelayForCreate = 5
 
 func create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	input := expandNiftyCreateSeparateInstanceRuleInput(d)
@@ -22,6 +25,9 @@ func create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.
 
 	SeparateInstanceRuleName := d.Get("name").(string)
 	d.SetId(SeparateInstanceRuleName)
+
+	// lintignore:R018
+	time.Sleep(waiterInitialDelayForCreate * time.Second)
 
 	return read(ctx, d, meta)
 }
