54a02674d870dd3c9d858209ed472674a55ec3d2
fuku <fukuzawa.makoto@fjct.fujitsu.com>
2021-08-31 13:41:05 +0900

    resource/nifcloud_elb: Add mutex lock for private lan

--- nifcloud/resources/network/elb/create.go
+++ nifcloud/resources/network/elb/create.go
@@ -8,14 +8,34 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/nifcloud/nifcloud-sdk-go/nifcloud"
 	"github.com/nifcloud/terraform-provider-nifcloud/nifcloud/client"
+	"github.com/nifcloud/terraform-provider-nifcloud/nifcloud/internal/mutexkv"
 )
 
 func create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	input := expandNiftyCreateElasticLoadBalancerInput(d)
 
 	svc := meta.(*client.Client).Computing
-	req := svc.NiftyCreateElasticLoadBalancerRequest(input)
 
+	for _, ni := range d.Get("network_interface").(*schema.Set).List() {
+		if v, ok := ni.(map[string]interface{}); ok {
+			if raw, ok := v["network_id"]; ok && len(raw.(string)) > 0 {
+				key, err := mutexkv.LockPrivateLan(ctx, raw.(string), svc)
+				if err != nil {
+					return diag.FromErr(err)
+				}
+				defer mutexkv.UnlockPrivateLan(key)
+			}
+			if raw, ok := v["network_name"]; ok && len(raw.(string)) > 0 {
+				key, err := mutexkv.LockPrivateLanByName(ctx, raw.(string), svc)
+				if err != nil {
+					return diag.FromErr(err)
+				}
+				defer mutexkv.UnlockPrivateLan(key)
+			}
+		}
+	}
+
+	req := svc.NiftyCreateElasticLoadBalancerRequest(input)
 	_, err := req.Send(ctx)
 	if err != nil {
 		return diag.FromErr(fmt.Errorf("failed creating elb: %s", err))
