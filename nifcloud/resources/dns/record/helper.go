3d0cfd98909847da6d0808d4aae4c673bc6e94b3
Sogo Kato <kato.sogo@fjct.fujitsu.com>
2023-03-31 11:27:51 +0900

    Change nifcloud_dns_record import interface to support shorthand record name

--- nifcloud/resources/dns/record/helper.go
+++ nifcloud/resources/dns/record/helper.go
@@ -8,16 +8,23 @@ import (
 )
 
 func validateDnsRecordImportString(importStr string) ([]string, error) {
-	// example: setIdentifier_example.test
+	// example: setIdentifier_example.test_@
+	// example: setIdentifier_example.test_sub
+	// example: setIdentifier_example.test_sub.example.test
 
 	importParts := strings.Split(importStr, "_")
-	errStr := "unexpected format of import string (%q), expected SETIDENTIFIER_ZONEID: %s"
-	if len(importParts) < 2 {
+	errStr := "unexpected format of import string (%q), expected SETIDENTIFIER_ZONEID_NAME: %s"
+	if len(importParts) < 3 {
 		return nil, fmt.Errorf(errStr, importStr, "invalid parts")
 	}
 
 	setIdentifier := importParts[0]
 	zoneID := importParts[1]
+	name := importParts[2]
+
+	if name == "" {
+		return nil, fmt.Errorf(errStr, importStr, "name must be required")
+	}
 
 	if zoneID == "" {
 		return nil, fmt.Errorf(errStr, importStr, "zone_id must be required")
@@ -33,6 +40,7 @@ func validateDnsRecordImportString(importStr string) ([]string, error) {
 func populateDnsRecordFromImport(d *schema.ResourceData, importParts []string) error {
 	setIdentifier := importParts[0]
 	zoneID := importParts[1]
+	name := importParts[2]
 
 	if err := d.Set("set_identifier", setIdentifier); err != nil {
 		return err
@@ -41,6 +49,10 @@ func populateDnsRecordFromImport(d *schema.ResourceData, importParts []string) e
 	d.SetId(setIdentifier)
 
 	if err := d.Set("zone_id", zoneID); err != nil {
+		return err
+	}
+
+	if err := d.Set("name", name); err != nil {
 		return err
 	}
 
